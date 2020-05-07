704642bd0dcd0b47114337f41fbf51c1d067c2ee
Lukas <lukas@colorfreedom.org>
2020-05-07 11:51:27 +0200

    fixed: issue with wildcard redirection

--- pymap-copy.py
+++ pymap-copy.py
@@ -16,7 +16,7 @@ parser.add_argument('-i', '--incremental', help='copy/creating only new folders/
 parser.add_argument('--abort-on-error', help='the process will interrupt at the first mail transfer error',
                     action="store_true")
 parser.add_argument('--denied-flags', help='mails with this flags will be skipped', type=str)
-parser.add_argument('-r', '--redirect', help='redirect a folder (source:destination)', action='append')
+parser.add_argument('-r', '--redirect', help='redirect a folder (source:destination --denied-flags seen,recent -d)', action='append')
 parser.add_argument('--ignore-quota', help='ignores insufficient quota', action='store_true')
 parser.add_argument('--ignore-folder-flags', help='do not link default IMAP folders automatically (like Drafts, '
                                                   'Trash, etc.)', action='store_true')
@@ -291,7 +291,7 @@ if args.redirect:
                 wildcard_matches = [f for f in db['source']['folders'] if f.startswith(r_source[:-1])]
                 if wildcard_matches:
                     for folder in wildcard_matches:
-                        redirections[folder] = '{}{}{}'.format(r_destination, destination_delimiter, folder)
+                        redirections[folder] = r_destination
                 else:
                     not_found.append(r_source)
             elif r_source not in db['source']['folders']:
